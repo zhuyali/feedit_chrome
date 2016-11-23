76066b190b36268a86ea848809c4781c8bb60861
zhuyali <495162889@qq.com>
2016-11-23 11:59:47 +0800

    tweak style

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,15 +3,21 @@
 window.onload = function() {
 
   var btn = document.getElementById('button');
+  var info = document.getElementById('info');
 
   btn.addEventListener('click', function() {
     chrome.runtime.sendMessage({
       type: 'add'
     }, function(response) {
-      if (response.success) {
-        window.close();
+      if (response != null) {
+        if (response.success) {
+          info.innerHTML = 'Success'
+          window.close();
+        } else {
+          info.innerHTML = 'Failed';
+        }
       } else {
-        alert('failed');
+        info.innerHTML = 'Lost Connection';
       }
     });
   });
